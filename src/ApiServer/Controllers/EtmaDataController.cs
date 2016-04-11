a33c16789c57c2601d5035943dfaefdbebb17cec
Anthony.Sawyer <Anthony.Sawyer@open.ac.uk>
2016-04-11 13:21:48 +0100

    Initial Commit

---
+++ src/ApiServer/Controllers/EtmaDataController.cs
@@ -0,0 +1,26 @@
+﻿namespace ApiServer.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNet.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [Route("api/[controller]")]
+    public class EtmaDataController : Controller
+    {
+        private readonly ILogger<EtmaDataController> logger;
+
+        public EtmaDataController(ILogger<EtmaDataController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            this.logger.LogInformation("Getting information for user <x>");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
