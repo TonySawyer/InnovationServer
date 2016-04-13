83087c9dd760f19cdac53764d94b91cd7edb73d0
Anthony.Sawyer <Anthony.Sawyer@open.ac.uk>
2016-04-13 15:28:05 +0100

    Use cors before MVC

--- src/ApiServer/Startup.cs
+++ src/ApiServer/Startup.cs
@@ -24,8 +24,8 @@
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
             services.AddCors();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
